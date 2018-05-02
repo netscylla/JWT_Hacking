0ff21aa2d8655ec2060cda0d9aa0516da7f7dc1d
netscylla <github@netscylla.com>
2018-05-02 21:50:57 +0100

    minor changes: added some error correction, and added help message

--- JWTCrack.py
+++ JWTCrack.py
@@ -7,26 +7,54 @@ import Queue
 import threading
 import jwt
 from termcolor import colored
+from optparse import OptionParser
 
 NumOfThreads=100
 queue = Queue.Queue()
 
-try:
-    encoded=sys.argv[1]
-    WordList=open(sys.argv[2],'r')
-except:
-    print "Usage: %s encoded wordlist" % sys.argv[0]
+def help_msg():
+    print """
+    ======================
+    JWTCrack
+    (c)2018 Netscylla
+    ======================
+    Disclaimer: This program is free to use at your own risk!
+                More details on the disclaimer and license available here: https://github.com/netscylla/JWT_Hacking
+                
+    Usage: JWTCrack.py [Encoded_JWT] [Wordlist] <Algorithm>"
+           Endcoded_JWT = Base64 Encoded JWT String
+           Wordlist     = Dictionary wordlist file used to bruteforce the JWT
+           Algorithm    = (Optional) HMAC Algorithm, default=HS256
+    """
     sys.exit(1)
+
+try: 
+    if (len(sys.argv) == 4):
+        encoded=sys.argv[1]
+        WordList=open(sys.argv[2],'r')
+        algorithm=sys.argv[3]
+    elif (len(sys.argv) == 3):
+        encoded=sys.argv[1]
+        WordList=open(sys.argv[2],'r')
+        algorithm="HS256"
+    else:
+        raise Exception
+except:
+    help_msg()
+    
 
 class checkHash(threading.Thread):
     def __init__(self,queue):
         threading.Thread.__init__(self)
         self.queue=queue
     def run(self):
+        options = ["HS256","HS384","HS512"]
+        if algorithm not in options: "HS256"
+    
         while True:
             self.secret=self.queue.get()
             try:
-                jwt.decode(encoded, self.secret, algorithms=['HS256'])
+                jwt.decode(encoded, self.secret, algorithm)
                 print colored('Success! ['+self.secret+']','green')
                 os._exit(0)
                 self.queue.task_done()
@@ -34,13 +62,19 @@ class checkHash(threading.Thread):
                 print colored('Invalid Token ['+self.secret+']','red')
             except jwt.ExpiredSignatureError:
                 print colored('Token Expired ['+self.secret+']','red')
+                
+    
+def main():    
 
-for i in range(NumOfThreads):
-    t=checkHash(queue)
-    t.setDaemon(True)
-    t.start()
+    for i in range(NumOfThreads):
+        t=checkHash(queue)
+        t.setDaemon(True)
+        t.start()
 
-for word in WordList.readlines():
-    queue.put(word.strip())
+    for word in WordList.readlines():
+        queue.put(word.strip())
 
-queue.join()
+    queue.join()
+
+if __name__ == '__main__':
+    main()
